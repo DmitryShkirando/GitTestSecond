059528dcc2692b43e4da9fc73608573afa1f16cc
NewAuthor <newauthor@example.com>
2025-02-24 15:25:24 +0400

    added new lines

--- TestGitSecondPart/UnitTest1.cs
+++ TestGitSecondPart/UnitTest1.cs
@@ -7,6 +7,8 @@ namespace TestGitSecondPart
         {
             Console.WriteLine("New line is added again");
             Console.WriteLine("New author and email");
+            Console.WriteLine("3-rd line is added again 2");
+            Console.WriteLine("3-rd line is added ag231ain 2");
         }
 
         [Test]
