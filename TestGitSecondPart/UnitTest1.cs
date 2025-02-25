d086ddde3671d6a6eb6359095f84f6907d0f3884
NewAuthor <newauthor@example.com>
2025-02-25 14:15:12 +0400

    push commit 1

--- TestGitSecondPart/UnitTest1.cs
+++ TestGitSecondPart/UnitTest1.cs
@@ -9,6 +9,7 @@ namespace TestGitSecondPart
             Console.WriteLine("New author and email");
             Console.WriteLine("3-rd line is added again 2");
             Console.WriteLine("3-rd line is added ag231ain 2");
+            Console.WriteLine("After reset");
         }
 
         [Test]
